7c4b0196522cb3fc4d9d2d04285dab7139f7705f
ClemTheStudent <47741953+ClemTheStudent@users.noreply.github.com>
2019-02-18 12:08:56 +0100

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClass
+{
+
+    class Customer
+    {
+
+        public Customer(string _firstName, string _lastName, string _city)
+        {
+            FirstName = _firstName;
+            LastName = _lastName;
+            City = _city;
+        }
+
+        public Customer()
+        {
+
+        }
+
+        /// <summary>
+        /// Méthode static
+        /// elle n'est pas appellée sur une instance en cours
+        /// Mais sur l'objet static Customer.
+        /// </summary>
+        public static int DifferenceAge(Customer c1, Customer c2)
+        {
+            return c2.Birthday.Year - c1.Birthday.Year;
+        }
+
+        public static Customer CreateNewCustomer()
+        {
+            return new Customer();
+        }
+
+
+        private string firstName;
+        private string lastName;
+        private DateTime birthday;
+
+        /// <summary>
+        /// Prénom de mon client
+        /// </summary>
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        /// <summary>
+        /// Nom de mon client
+        /// </summary>
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        /// <summary>
+        /// Date de naissance
+        /// </summary>
+        public DateTime Birthday
+        {
+            get
+            {
+                return birthday;
+            }
+            set
+            {
+                birthday = value;
+            }
+        }
+
+        public String City
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Renvoit le prénom, nom et ville
+        /// </summary>
+        /// <returns></returns>
+        public String GetCompleteName()
+        {
+
+            return string.Format("Mon client : {0} {1} Ville : {2}", FirstName, LastName, City);
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            // Instanciation d'un objet de type (class) Customer
+            Customer client1 = new Customer("Sébastien", "Pertus", "Toulouse");
+            client1.Birthday = new DateTime(1976, 10, 23);
+            Console.WriteLine(client1.GetCompleteName());
+
+            Customer client2 = new Customer();
+            //Customer client2 = Customer.CreateNewCustomer();
+
+            client2.City = "Paris";
+            client2.FirstName = "Jean Sébastien";
+            client2.LastName = "Dupuys";
+            client2.Birthday = new DateTime(1977, 11, 10);
+            Console.WriteLine(client2.GetCompleteName());
+
+            var dif = Customer.DifferenceAge(client1, client2);
+
+            Console.WriteLine(dif.ToString());
+
+
+            Console.Read();
+
+
+        }
+    }
+
+}
